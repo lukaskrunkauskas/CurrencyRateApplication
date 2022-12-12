aea8494a3370f5f5a6b59ecc5629c3da8f908f52
lukas <lukaskrun@gmail.com>
2022-12-12 19:28:53 +0200

    Added tests

--- CurrencyRateApplication/src/test/java/com/currencyrateapplication/currencyrateapplication/CurrencyRateApplicationTests.java
+++ CurrencyRateApplication/src/test/java/com/currencyrateapplication/currencyrateapplication/CurrencyRateApplicationTests.java
@@ -2,12 +2,39 @@ package com.currencyrateapplication.currencyrateapplication;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.xml.sax.SAXException;
+
+import javax.xml.parsers.ParserConfigurationException;
+import java.io.IOException;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 @SpringBootTest
 class CurrencyRateApplicationTests {
+
+	CurrencyDataSource currencyDataSource = new CurrencyDataSource();
 
 	@Test
 	void contextLoads() {
 	}
 
+	@Test
+	void testGetLatestRates() throws ParserConfigurationException, IOException, SAXException {
+		String url = "http://www.lb.lt/webservices/FxRates/FxRates.asmx/getCurrentFxRates?tp=eu";
+		assertThat(currencyDataSource.getLatestRates(url)).isNotEmpty();
+	}
+
+	@Test
+	void testCalculateCurrency() throws ParserConfigurationException, IOException, SAXException {
+		String url = "http://www.lb.lt/webservices/FxRates/FxRates.asmx/getCurrentFxRates?tp=eu";
+		String ccyFrom = "USD";
+		String sum = "100";
+		String ccyTo = "EUR";
+		currencyDataSource = new CurrencyDataSource();
+
+		double answer = currencyDataSource.calculateCurrency(url, ccyFrom, sum, ccyTo);
+
+		assertTrue(answer == 94.6790380609733);
+	}
 }
